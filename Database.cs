9b03bfa4febf8c093f66e823c0ab736c0fd5075e
Rius2g <marius.mollerhansen@hotmail.com>
2023-03-04 05:01:34 +0100

    some database refresh stuff

--- Database.cs
+++ Database.cs
@@ -109,6 +109,35 @@ namespace Database //do all the setup and functions for database
             connection.Close();
         }
 
+        public void NewPrice( DateTime date) //function to update the price of all securities if date is different from last update
+        {
+
+            //first fetch the entire list and see if any are outdated 
+            List<Modules.Security> securities = GetSecurities();
+            foreach (Modules.Security security in securities)
+            {
+                if (security.Date != date)
+                {
+                    //fetch the price of the security
+                    double price = 0;
+                    if (security.Type == 1) //crypto
+                    {
+                        price = API.Get.GetCryptoPrice(security.Ticker).Result;
+                        string priceString = price.ToString();
+                        price = Convert.ToDouble(priceString);
+                    }
+                    else if (security.Type == 2 || security.Type == 3 || security.Type == 6) //stock, etf, index fund
+                    price = API.Get.GetStockPrice(security.Ticker).Result;
+                    else if (security.Type == 5) //mutual fund
+                    price = API.Get.GetMutualFundPrice(security.Ticker).Result;
+
+                    //update the price
+                    UpdateSecurity(security.Ticker, (float)price, security.Quantity, date, DateTime.Now, security.Type.ToString());
+                }
+            }
+            // Create a new database connection:
+        }
+
 
         public void PurgeDatabase()
         {
@@ -129,7 +158,7 @@ namespace Database //do all the setup and functions for database
             connection.Close();
         }
 
-        public List<Modules.DisplayedSecurity> GetSecurities()
+        public List<Modules.DisplayedSecurity> GetDisplayedSecurities()
         {
             // Create a new database connection:
             var connectionStringBuilder = new SqliteConnectionStringBuilder();
@@ -148,6 +177,34 @@ namespace Database //do all the setup and functions for database
                 while (reader.Read())
                 {
                     Modules.DisplayedSecurity security = new Modules.DisplayedSecurity(reader.GetString(0), reader.GetFloat(1), reader.GetInt16(2), reader.GetInt16(3));
+                    securities.Add(security);
+                }
+                return securities;
+            }
+        }
+
+        public List<Modules.Security> GetSecurities()
+        {
+            // Create a new database connection:
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "Holdings.db";
+            var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);
+
+            // Open the connection:
+            connection.Open();
+
+            // Read some data:
+            var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = "SELECT Ticker, Price, Quantity, Date, Time, Type FROM Securities";
+            using (var reader = selectCommand.ExecuteReader())
+            {
+                List<Modules.Security> securities = new List<Modules.Security>();
+                while (reader.Read())
+                {
+                    Modules.Security security = new Modules.Security(reader.GetString(0), reader.GetInt16(2), reader.GetInt16(5));
+                    security.Date = reader.GetDateTime(3);
+                    security.Time = reader.GetDateTime(4);
+                    security.Price = reader.GetFloat(1);
                     securities.Add(security);
                 }
                 return securities;
